fe9e2f4573af7d31822f0564e41d54ee13a9faec
YumiKim <dwkimym93@gmail.com>
2021-12-06 23:38:47 +0900

    💄 Add UI and list JSX

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import './App.css';
+import Habit from './components/habit';
 
 function App() {
-  const name = undefined;
-  return (
-    <>
-      <h1>hello</h1>
-  {name && <h2>Hi! {name}</h2>}
-  {
-  ['apple', 'carrot'].map(item => <h2>{item}</h2>)
-}  
-  </>
-  );
+  return <Habit />;
 }
 
 export default App;
