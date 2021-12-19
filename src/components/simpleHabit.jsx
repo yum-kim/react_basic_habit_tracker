dc4b238d25503cc63a98b137d7114fd9778262f1
YumiKim <dwkimym93@gmail.com>
2021-12-19 21:57:08 +0900

    🎨 Practice Hooks

---
+++ src/components/simpleHabit.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useCallback, useRef, useEffect } from 'react';
+
+const SimpleHabit = (props) => {
+  const [count, setCount] = useState(0);
+  const spanRef = useRef();
+
+  const handleIncrement = useCallback(() => {
+    setCount(count + 1);
+  });
+
+  useEffect(() => {
+    console.log(`mounted & updated ${count}`);
+  }, [count]);
+
+  return (
+    <li className="habit">
+      <span ref={spanRef} className="habit-name">
+        Reading
+      </span>
+      <span className="habit-count">{count}</span>
+      <button className="habit-button habit-increase" onClick={handleIncrement}>
+        <i className="fas fa-plus-square"></i>
+      </button>
+    </li>
+  );
+};
+
+export default SimpleHabit;
